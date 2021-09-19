3e50c423e8b751b1f459b124097cbc443f55db11
nkt.brem <nkt.brem@gmail.com>
2021-09-19 16:42:27 +0300

    initial commit

---
+++ scripts/carousel.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+       
+    //-------------------------------- Carousel Experiences
+
+    $("#exp-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        checkVisible: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplaySpeed: 7,
+        responsiveRefreshRate: 50,
+    });
+
+    //-------------------------------- Carousel Team
+
+    $("#team-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        checkVisible: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplaySpeed: 7,
+        responsiveRefreshRate: 50,
+    });
+  });
---
+++ scripts/mobile-menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    $("#mobile-menu__btn").click(function(){
+        $("#mobile-menu").css("display", "block")
+    });
+
+    $("#mobile-menu__close-btn").click(function(){
+        $("#mobile-menu").css("display", "none")
+    });
+ 
+  });
